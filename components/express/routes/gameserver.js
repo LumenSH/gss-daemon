31a0ac65515641b2c3b5c1928277fff4caba6c59
nevo <nevo@posteo.de>
2017-07-19 15:22:13 -0400

    Added CPU and memory usage to gameserver API method for future commit

--- components/express/routes/gameserver.js
+++ components/express/routes/gameserver.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const pusage = require('pidusage');
+
 module.exports = {
     list: (req, res) => {
         return res.Response.setData(Object.keys(gameserverManager.servers)).output();
@@ -7,7 +9,23 @@ module.exports = {
     get: (req, res) => {
         if(req.params.id !== undefined) {
             if(gameserverManager.servers[req.params.id] !== undefined) {
-                return res.Response.setData(gameserverManager.servers[req.params.id]).setStatus(true).output();
+                let server = gameserverManager.servers[req.params.id];
+                server.usage = {
+                    cpu: 0,
+                    mem: 0
+                };
+                pusage.stat(server.process.pid, (err, stat) => {
+                    if(!err) {
+                        server.usage = {
+                            cpu: stat.cpu,
+                            mem: helper.byteToMb(stat.memory)
+                        };
+                    }
+                    console.error(`Error while getting PID usage for process ${server.process.pid}: ${err.toString()}`);
+                    return res.Response.setData(server).setStatus(true).output();
+                });
+                pusage.unmonitor(server.process.pid);
+                return res.Response.setData(server).setStatus(true).output();
             }
         }
         return res.Response.output();
