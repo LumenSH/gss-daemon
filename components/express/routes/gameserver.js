11035ea497557f175bcdc0f8f6eb2992e3254171
xNevo <nevo@posteo.de>
2017-07-22 22:07:20 +0200

    Converting circular structure to JSON

--- components/express/routes/gameserver.js
+++ components/express/routes/gameserver.js
@@ -10,20 +10,28 @@ module.exports = {
         if(req.params.id !== undefined) {
             if(gameserverManager.servers[req.params.id] !== undefined) {
                 let server = gameserverManager.servers[req.params.id];
-                server.usage = {
-                    cpu: 0,
-                    mem: 0
+                let data = {
+                    id: server.gameserver.id,
+                    userID: server.gameserver.userID,
+                    uid: server.uid,
+                    path: server.path,
+                    usage: {
+                        mem: 0,
+                        cpu: 0,
+                    },
+                    pid: server.process.pid,
+                    started_at: server.started_at
                 };
                 pusage.stat(server.process.pid, (err, stat) => {
                     if(!err) {
-                        server.usage = {
+                        data.usage = {
                             cpu: stat.cpu,
                             mem: helper.byteToMb(stat.memory)
                         };
                     } else {
                         console.error(`Error while getting PID usage for process ${server.process.pid}: ${err.toString()}`);
                     }
-                    return res.Response.setData(server).setStatus(true).output();
+                    return res.Response.setData(data).setStatus(true).output();
                 });
                 pusage.unmonitor(server.process.pid);
                 return;
