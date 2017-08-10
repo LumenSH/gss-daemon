c824b576ca8344298b81aa38232132061cebb842
xNevo <nevo@posteo.de>
2017-08-11 00:47:03 +0200

    Fix API timeout on requesting not loaded server

--- components/express/routes/gameserver.js
+++ components/express/routes/gameserver.js
@@ -10,31 +10,35 @@ module.exports = {
         if(req.params.id !== undefined) {
             if(gameserverManager.servers[req.params.id] !== undefined) {
                 let server = gameserverManager.servers[req.params.id];
-                let data = {
-                    id: server.gameserver.id,
-                    userID: server.gameserver.userID,
-                    uid: server.uid,
-                    path: server.path,
-                    gameserver: server.gameserver,
-                    usage: {
-                        mem: 0,
-                        cpu: 0,
-                    },
-                    pid: server.process.pid,
-                    started_at: server.started_at
-                };
-                pusage.stat(server.process.pid, (err, stat) => {
-                    if(!err) {
-                        data.usage = {
-                            cpu: stat.cpu,
-                            mem: helper.byteToMb(stat.memory)
-                        };
-                    } else {
-                        console.error(`Error while getting PID usage for process ${server.process.pid}: ${err.toString()}`);
-                    }
-                    res.ApiResponse.setData(data).setStatus(true).output();
-                });
-                pusage.unmonitor(server.process.pid);
+                if (server.isRunning()) {
+                    let data = {
+                        id: server.gameserver.id,
+                        userID: server.gameserver.userID,
+                        uid: server.uid,
+                        path: server.path,
+                        gameserver: server.gameserver,
+                        usage: {
+                            mem: 0,
+                            cpu: 0,
+                        },
+                        pid: server.process.pid,
+                        started_at: server.started_at
+                    };
+                    pusage.stat(server.process.pid, (err, stat) => {
+                        if (!err) {
+                            data.usage = {
+                                cpu: stat.cpu,
+                                mem: helper.byteToMb(stat.memory)
+                            };
+                        } else {
+                            console.error(`Error while getting PID usage for process ${server.process.pid}: ${err.toString()}`);
+                        }
+                        res.ApiResponse.setData(data).setStatus(true).output();
+                    });
+                    pusage.unmonitor(server.process.pid);
+                } else {
+                    res.ApiResponse.setStatus(false).output();
+                }
             }
         }
     },
