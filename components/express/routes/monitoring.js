2bec919abf99ddd711c91027523b7c930f7a2dd3
xNevo <nevo@posteo.de>
2017-08-12 12:23:42 +0200

    Fix monitoring endpoint displaying wrong memory informations

--- components/express/routes/monitoring.js
+++ components/express/routes/monitoring.js
@@ -1,28 +1,31 @@
 'use strict';
 
-const os = require('os');
+const   os = require('os'),
+        si = require('systeminformation');
 
 module.exports = (req, res) => {
     let cpus = os.cpus();
-    res.ApiResponse.setStatus(true).setPayload({
-        hostname: os.hostname(),
-        cpu: {
-            loadavg: os.loadavg(),
-            architecture: os.arch(),
-            model: cpus[0].model,
-            cores: cpus.length
-        },
-        mem: {
-            total: helper.byteToMb(os.totalmem()),
-            available: helper.byteToMb(os.freemem()),
-            in_use: helper.byteToMb(os.totalmem() - os.freemem())
-        },
-        uptime: {
-            daemon: process.uptime().toFixed(0),
-            sys: os.uptime().toFixed(0)
-        },
-        servers: Object.keys(gameserverManager.servers).length,
-        tokens: Object.keys(ioManager.tokens).length,
-        sockets: Object.keys(ioManager.clients).length
-    }).output();
+    si.mem((mem_data) => {
+        res.ApiResponse.setStatus(true).setPayload({
+            hostname: os.hostname(),
+            cpu: {
+                loadavg: os.loadavg(),
+                architecture: os.arch(),
+                model: cpus[0].model,
+                cores: cpus.length
+            },
+            mem: {
+                total: helper.byteToMb(mem_data.total),
+                available: helper.byteToMb(mem_data.free),
+                in_use: helper.byteToMb(mem_data.total - mem_data.free)
+            },
+            uptime: {
+                daemon: process.uptime().toFixed(0),
+                sys: os.uptime().toFixed(0)
+            },
+            servers: Object.keys(gameserverManager.servers).length,
+            tokens: Object.keys(ioManager.tokens).length,
+            sockets: Object.keys(ioManager.clients).length
+        }).output();
+    });
 };
